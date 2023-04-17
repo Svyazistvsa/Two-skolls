a4fafc34fb84530cff4f97d67603163cd9db6994
Svyazistvsa <sviazist.vsa@gmail.com>
2023-04-17 21:00:33 +0500

    Был бухой

--- js/end-game.js
+++ js/end-game.js
@@ -1,1 +1,15 @@
 "use strict"
+import{delFlip } from "./menu.js"
+
+function endGame(points){
+    delFlip();
+    let exprSrc = document.createElement("div"),
+        score = document.createElement("div"),
+        endScr = document.createElement("div");
+    endScr.classList.add("screen");
+    exprSrc.classList.add("expr");
+    score.classList.add("score");
+    score.innerHTML = score;
+
+    document.querySelector(".game");
+}
