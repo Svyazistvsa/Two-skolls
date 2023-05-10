33da176a0f02803198aece346fb6ddf55b2d71e0
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-05-10 15:32:54 +0500

    huy

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-let menu = document.querySelector('#main_menu'),
-    list = document.querySelector('.list'),
-    container = document.querySelector('#container');    
-
-menu.onclick = () => list.classList.toggle('block');
