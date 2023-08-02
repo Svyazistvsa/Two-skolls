4a92f4d9b1673945995481f359620e075dc92f38
Svyazist <sviazist.vsa@gmail.com>
2023-08-02 21:39:19 +0500

    start batton

--- js/startButton.js
+++ js/startButton.js
@@ -2,4 +2,23 @@
 
 import {render} from "./game.js";
 
-let startButton = document.querySelector(".startButton");
+let startButton = document.querySelector(".startButton"),
+    form = document.forms.variant,
+    variantCells = form.elements.cells,
+    cells;
+
+    for(let x=0; x<variantCells.lenght; x+1){
+        if(variantCells[x].checked) cells=variantCells[x].value;
+        alert(cells);
+    }
+
+    startButton.addEventListener("click", quickStart);
+
+    function quickStart(){
+        render(game, cells+0, "titular");    
+        
+    }
+
+    
+
+    
