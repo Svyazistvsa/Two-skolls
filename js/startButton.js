d80c6c01d5f65660b5f21e2ac1e545366af74f74
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-08-07 07:42:27 +0500

    How get collection lenght

--- js/startButton.js
+++ js/startButton.js
@@ -3,21 +3,19 @@
 import {render} from "./game.js";
 
 let startButton = document.querySelector(".startButton"),
-    form = document.forms.variant;
-
+    form = document.forms.variant, 
+    variantCells = form.elements.cells,
+    variantArr = Array.from(variantCells);
     
-
     startButton.addEventListener("click", quickStart);
 
     function quickStart(){
-        let cell,
-            variantCells = form.elements.cells;
-            alert(variantCells);
-        for(let x=0; x<variantCells.lenght; x+1){
+        let cell;
+alert(variantArr.lenght);
+        for(let x=0; x<variantArr.lenght; x++){
           //  alert(x);
-            if(variantCells[x].checked) cell=variantCells[x].value;
+            if(variantArr[x].checked) cell=variantArr[x].value;
         }
         //alert(cell);
         render(game, cell, "titular");    
-        
     }
