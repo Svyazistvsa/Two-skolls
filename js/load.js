908c240dad49b3f9ec17a6caa822efcc93fe99ed
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-08-28 11:57:50 +0500

    not audio

---
+++ js/load.js
@@ -0,0 +1,25 @@
+"use strict"
+
+import { jpgArr } from "./game.js";
+preloadImg(jpgArr);
+window.addEventListener('load', () => {
+  
+  document.querySelector("#load").classList.add("none");
+})
+
+function preloadImg(arr){
+    for(let i = 0; i < arr.length; i++){
+      let image = new Image();
+      image.src = arr[i].img;
+    }
+    let shirt = new Image();
+    shirt.src = "jpg/shirt0.jpg";
+    let woll = new Image();
+    woll.src = "jpg/wollpaper10.jpg";
+    let win = new Image();
+    win.src = "jpg/winner.jpg";
+    let may = new Image();
+    may.src = "jpg/maybe.jpg";
+    let over = new Image();
+    over.src = "jpg/gameover.jpg";  
+}
