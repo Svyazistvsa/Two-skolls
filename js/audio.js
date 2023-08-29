377e3e77c155f23b0ed248f2f764c52e7a250cba
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-08-29 13:35:21 +0500

    create audio function

--- js/audio.js
+++ js/audio.js
@@ -1,3 +1,15 @@
 "use strict"
 
 /*Написать функцию проигрывания музыки и импортировать*/
+function playing(track){
+  track.play();
+}
+
+function insertAudio(track, area){
+  area.insertAdjasentHTML("afterbegin", `
+    <audio>
+      <source src="audio/${track}.ogg" type="audio/ogg" >
+      <source src="audio/${track}.mp3" type="audio/mpeg" >
+    </audio>
+  `)
+}
