1263c212de11f0627cf8064e4cf62ae92c244c31
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-08-29 15:36:42 +0500

    sound is work 60%

--- js/audio.js
+++ js/audio.js
@@ -2,12 +2,12 @@
 
 /*Написать функцию проигрывания музыки и импортировать*/
 function playing(track){
-  track.play();
+  document.querySelector(`#${track}`).play();
 }
 
 function insertAudio(track, area){
-  area.insertAdjasentHTML("afterbegin",
-  `<audio>
+  area.insertAdjacentHTML("afterbegin",
+  `<audio id="${track}" >
       <source src="audio/${track}.ogg" type="audio/ogg" >
       <source src="audio/${track}.mp3" type="audio/mpeg" >
     </audio>`
