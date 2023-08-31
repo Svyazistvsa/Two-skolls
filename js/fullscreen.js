804201c3aa9c27134d979dc3112032ab9d0ade5f
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-08-31 16:03:15 +0500

    start fullscreen

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -4,6 +4,8 @@ let full = document.querySelector("#full"),
     
 full.addEventListener("click", thatFull);
 
+
+
 function thatFull(){
   if(!document.fullscreenElement){
     document.documentElement.requestFullscreen();
